485f994665f238310ac4cfdff860aee02b6ae031
Kenju Wagatsuma <ken901waga@gmail.com>
2019-01-01 12:41:29 +0900

    add GeneratorInt

--- main_test.go
+++ main_test.go
@@ -6,11 +6,20 @@ import (
 	"testing"
 )
 
+func TestGeneratorInt(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	for v := range GeneratorInt(ctx, 1, 2, 3, 4) {
+		fmt.Printf("%v ", v)
+	}
+}
+
 func TestRepeat(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	for num := range Take(ctx, Repeat(ctx, 1), 10) {
-		fmt.Printf("%v ", num)
+	for v := range Take(ctx, Repeat(ctx, 1), 10) {
+		fmt.Printf("%v ", v)
 	}
 }
