2ca4768ddd06aacb67dd897f19bb44bd86ed3e4f
Kenju Wagatsuma <ken901waga@gmail.com>
2019-01-02 21:39:35 +0900

    add comment to genny.go

--- genny.go
+++ genny.go
@@ -4,4 +4,10 @@ import (
 	"github.com/cheekybits/genny/generic"
 )
 
+// GenType is used for `go generate`, which is offered by github.com/cheekybits/genny.
+//
+// Add specific comments (which starts with "//go:generate ..." into the source code
+// and run `go generate`.
+//
+// Read https://github.com/cheekybits/genny for more details.
 type GenType generic.Type
