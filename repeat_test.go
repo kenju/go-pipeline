afe9bb6ea93388355ab4780a3eece2cc4d2c94f7
Kenju Wagatsuma <ken901waga@gmail.com>
2019-01-04 11:31:30 +0900

    add RepeatString, RepeatInt, RepeatFloat32, RepeatFnString, RepeatFnInt, RepeatFnFloat32

--- repeat_test.go
+++ repeat_test.go
@@ -23,6 +23,63 @@ func TestRepeatFn(t *testing.T) {
 
 	randFn := func() interface{} { return rand.Int() }
 	for v := range pipeline.Take(ctx, pipeline.RepeatFn(ctx, randFn), 5) {
-		fmt.Println(v)
+		fmt.Printf("%v ", v)
+	}
+}
+
+func TestRepeatString(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	for v := range pipeline.TakeString(ctx, pipeline.RepeatString(ctx, "hello"), 10) {
+		fmt.Printf("%v ", v)
+	}
+}
+
+func TestRepeatFnString(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	repeatFn := func() string { return "!" }
+	for v := range pipeline.TakeString(ctx, pipeline.RepeatFnString(ctx, repeatFn), 5) {
+		fmt.Printf("%v ", v)
+	}
+}
+
+func TestRepeatInt(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	for v := range pipeline.TakeInt(ctx, pipeline.RepeatInt(ctx, 1), 10) {
+		fmt.Printf("%v ", v)
+	}
+}
+
+func TestRepeatFnInt(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	repeatFn := func() int { return 9 }
+	for v := range pipeline.TakeInt(ctx, pipeline.RepeatFnInt(ctx, repeatFn), 5) {
+		fmt.Printf("%v ", v)
+	}
+}
+
+func TestRepeatFloat32(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	for v := range pipeline.TakeFloat32(ctx, pipeline.RepeatFloat32(ctx, 1.1), 10) {
+		fmt.Printf("%v ", v)
+	}
+}
+
+func TestRepeatFnFloat32(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	repeatFn := func() float32 { return 1.1 }
+	for v := range pipeline.TakeFloat32(ctx, pipeline.RepeatFnFloat32(ctx, repeatFn), 5) {
+		fmt.Printf("%v ", v)
 	}
 }
