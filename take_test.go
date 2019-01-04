d187122d580452538cf9c38e24f5579274d7986e
Kenju Wagatsuma <ken901waga@gmail.com>
2019-01-04 10:52:57 +0900

    add TakeInt, TakeString and TakeFloat32

--- take_test.go
+++ take_test.go
@@ -15,3 +15,30 @@ func TestTake(t *testing.T) {
 		fmt.Printf("%v ", v)
 	}
 }
+
+func TestTakeInt(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	for v := range pipeline.TakeInt(ctx, pipeline.GeneratorInt(ctx, 1, 2, 3), 1) {
+		fmt.Printf("%v ", v)
+	}
+}
+
+func TestTakeString(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	for v := range pipeline.TakeString(ctx, pipeline.GeneratorString(ctx, "hello", "world", "!"), 1) {
+		fmt.Printf("%v ", v)
+	}
+}
+
+func TestTakeFloat32(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	for v := range pipeline.TakeFloat32(ctx, pipeline.GeneratorFloat32(ctx, 1.1, 2.2, 3.3), 1) {
+		fmt.Printf("%v ", v)
+	}
+}
