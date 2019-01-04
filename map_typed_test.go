f5cbed7f12974612f17b00d6d28e4e75b6bdeecb
Kenju Wagatsuma <ken901waga@gmail.com>
2019-01-04 19:28:57 +0900

    write test for map_typed_test.go

--- map_typed_test.go
+++ map_typed_test.go
@@ -2,18 +2,25 @@ package pipeline_test
 
 import (
 	"testing"
-	"fmt"
 	"context"
 	"github.com/kenju/go-pipeline"
+	"reflect"
+	"math"
 )
 
 func TestMapInt(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	var results []int
 	mapFn := func(v int) int { return v + 1 }
 	for v := range pipeline.MapInt(ctx, mapFn, pipeline.GeneratorInt(ctx, 1, 2, 3, 4)) {
-		fmt.Println(v)
+		results = append(results, v)
+	}
+
+	expected := []int{2, 3, 4, 5}
+	if !reflect.DeepEqual(results, expected) {
+		t.Errorf("expected %v, got %v", expected, results)
 	}
 }
 
@@ -21,9 +28,15 @@ func TestMapString(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	mapFn := func(v string) string { return "**" + v + "**"}
+	var results []string
+	mapFn := func(v string) string { return "**" + v + "**" }
 	for v := range pipeline.MapString(ctx, mapFn, pipeline.GeneratorString(ctx, "hello", "world")) {
-		fmt.Println(v)
+		results = append(results, v)
+	}
+
+	expected := []string{"**hello**", "**world**"}
+	if !reflect.DeepEqual(results, expected) {
+		t.Errorf("expected %v, got %v", expected, results)
 	}
 }
 
@@ -31,8 +44,20 @@ func TestMapFloat32(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	var results []float32
 	mapFn := func(v float32) float32 { return v + 0.1 }
 	for v := range pipeline.MapFloat32(ctx, mapFn, pipeline.GeneratorFloat32(ctx, 1.1, 2.2, 3.3)) {
-		fmt.Println(v)
+		results = append(results, v)
 	}
+
+	expected := []float32{1.2, 2.3, 3.3}
+	for i, v := range results {
+		if !float32AlmostEqual(v, expected[i]) {
+			t.Errorf("expected %.6f, got %.6f", expected[i], v)
+		}
+	}
+}
+
+func float32AlmostEqual(a, b float32) bool {
+	return math.Abs(float64(a) - float64(b)) <= 1e-1
 }
