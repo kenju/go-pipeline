b4cfd765fb83602eddbc5b889a39a683d5c0f331
Kenju Wagatsuma <ken901waga@gmail.com>
2019-01-04 21:17:42 +0900

    write test for take_test.go

--- take_test.go
+++ take_test.go
@@ -3,16 +3,22 @@ package pipeline_test
 import (
 	"testing"
 	"github.com/kenju/go-pipeline"
-	"fmt"
-	"context"
+		"context"
+	"reflect"
 )
 
 func TestTake(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	var results []interface{}
 	for v := range pipeline.Take(ctx, pipeline.Repeat(ctx, 1), 3) {
-		fmt.Printf("%v ", v)
+		results = append(results, v)
+	}
+
+	expected := []interface{}{1, 1, 1}
+	if !reflect.DeepEqual(results, expected) {
+		t.Errorf("expected %v, got %v", expected, results)
 	}
 }
 
@@ -20,8 +26,14 @@ func TestTakeInt(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	var results []int
 	for v := range pipeline.TakeInt(ctx, pipeline.GeneratorInt(ctx, 1, 2, 3), 1) {
-		fmt.Printf("%v ", v)
+		results = append(results, v)
+	}
+
+	expected := []int{1}
+	if !reflect.DeepEqual(results, expected) {
+		t.Errorf("expected %v, got %v", expected, results)
 	}
 }
 
@@ -29,8 +41,14 @@ func TestTakeString(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	var results []string
 	for v := range pipeline.TakeString(ctx, pipeline.GeneratorString(ctx, "hello", "world", "!"), 1) {
-		fmt.Printf("%v ", v)
+		results = append(results, v)
+	}
+
+	expected := []string{"hello"}
+	if !reflect.DeepEqual(results, expected) {
+		t.Errorf("expected %v, got %v", expected, results)
 	}
 }
 
@@ -38,7 +56,13 @@ func TestTakeFloat32(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	var results []float32
 	for v := range pipeline.TakeFloat32(ctx, pipeline.GeneratorFloat32(ctx, 1.1, 2.2, 3.3), 1) {
-		fmt.Printf("%v ", v)
+		results = append(results, v)
+	}
+
+	expected := []float32{1.1}
+	if !reflect.DeepEqual(results, expected) {
+		t.Errorf("expected %v, got %v", expected, results)
 	}
 }
