487b217a858c537617fcdbf2b402e06c5343d34f
Kenju Wagatsuma <ken901waga@gmail.com>
2019-01-04 12:27:01 +0900

    add FanInString, FanInInt and FanInFloat32

--- fan_in_test.go
+++ fan_in_test.go
@@ -21,3 +21,48 @@ func TestFanIn(t *testing.T) {
 		fmt.Println(v)
 	}
 }
+
+func TestFanInString(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	numChannels := 3
+	channels := make([]<-chan string, numChannels)
+	for i := 0; i < numChannels; i++ {
+		channels[i] = pipeline.RepeatString(ctx, fmt.Sprintf("id:%d", i))
+	}
+
+	for v := range pipeline.TakeString(ctx, pipeline.FanInString(ctx, channels...), 10) {
+		fmt.Println(v)
+	}
+}
+
+func TestFanInInt(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	numChannels := 3
+	channels := make([]<-chan int, numChannels)
+	for i := 0; i < numChannels; i++ {
+		channels[i] = pipeline.RepeatInt(ctx, i)
+	}
+
+	for v := range pipeline.TakeInt(ctx, pipeline.FanInInt(ctx, channels...), 10) {
+		fmt.Println(v)
+	}
+}
+
+func TestFanInFloat32(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	numChannels := 3
+	channels := make([]<-chan float32, numChannels)
+	for i := 0; i < numChannels; i++ {
+		channels[i] = pipeline.RepeatFloat32(ctx, float32(i + 1) * 0.1)
+	}
+
+	for v := range pipeline.TakeFloat32(ctx, pipeline.FanInFloat32(ctx, channels...), 10) {
+		fmt.Println(v)
+	}
+}
