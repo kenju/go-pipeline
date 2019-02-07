c9dd4417a45f36f7ca114db9886dd99ef57dd713
Kenju Wagatsuma <ken901waga@gmail.com>
2019-02-08 08:21:29 +0900

    rollback RepeatFn test

--- repeat_test.go
+++ repeat_test.go
@@ -21,3 +21,19 @@ func TestRepeatGenType(t *testing.T) {
 		t.Errorf("expected %v, got %v", expected, results)
 	}
 }
+
+func TestRepeatFnGenType(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	var results []pipeline.GenType
+	repeatFn := func() pipeline.GenType { return 3 }
+	for v := range pipeline.TakeGenType(ctx, pipeline.RepeatFnGenType(ctx, repeatFn), 5) {
+		results = append(results, v)
+	}
+
+	expected := []pipeline.GenType{3, 3, 3, 3, 3}
+	if !reflect.DeepEqual(results, expected) {
+		t.Errorf("expected %v, got %v", expected, results)
+	}
+}
