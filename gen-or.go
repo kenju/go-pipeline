6fab4f9372f0653f8d583a941aec0aaa3d72f2b7
Kenju Wagatsuma <ken901waga@gmail.com>
2019-02-10 12:13:05 +0900

    support uint for Or

--- gen-or.go
+++ gen-or.go
@@ -179,6 +179,41 @@ func OrInt(
 	return orDone
 }
 
+// OrUint return only the first result from multiple channels.
+// Use ctx to cancel the stream processing.
+func OrUint(
+	channels ...<-chan uint,
+) <-chan uint {
+	switch len(channels) {
+	case 0: // when there are no channels to handle
+		return nil
+	case 1: // when there is only one channel in the list
+		return channels[0]
+	}
+
+	orDone := make(chan uint)
+	go func() {
+		defer close(orDone)
+
+		switch len(channels) {
+		case 2:
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			}
+		default:
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			case <-channels[2]:
+			case <-OrUint(append(channels[3:], orDone)...):
+				// here, recursively call or() to after the 3rd channel
+			}
+		}
+	}()
+	return orDone
+}
+
 // OrUint64 return only the first result from multiple channels.
 // Use ctx to cancel the stream processing.
 func OrUint64(
--- or_test.go
+++ or_test.go
@@ -6,9 +6,9 @@ import (
 	"time"
 )
 
-func ExampleOrInterface() {
-	sig := func(after time.Duration) <-chan interface{} {
-		c := make(chan interface{})
+func ExampleOrGenType() {
+	sig := func(after time.Duration) <-chan pipeline.GenType {
+		c := make(chan pipeline.GenType)
 		go func() {
 			defer close(c)
 			time.Sleep(after)
@@ -17,7 +17,7 @@ func ExampleOrInterface() {
 	}
 
 	start := time.Now()
-	channels := []<-chan interface{}{
+	channels := []<-chan pipeline.GenType{
 		sig(2 * time.Hour),
 		sig(5 * time.Minute),
 		sig(1 * time.Second), // the close message of this channel will close the whole channels
@@ -25,166 +25,6 @@ func ExampleOrInterface() {
 		sig(1 * time.Minute),
 	}
 
-	<-pipeline.OrInterface(channels...)
-	fmt.Printf("done after %v", time.Since(start))
-}
-
-func ExampleOrBool() {
-	sig := func(after time.Duration) <-chan bool {
-		c := make(chan bool)
-		go func() {
-			defer close(c)
-			time.Sleep(after)
-		}()
-		return c
-	}
-
-	start := time.Now()
-	channels := []<-chan bool{
-		sig(2 * time.Hour),
-		sig(5 * time.Minute),
-		sig(1 * time.Second), // the close message of this channel will close the whole channels
-		sig(1 * time.Hour),
-		sig(1 * time.Minute),
-	}
-
-	<-pipeline.OrBool(channels...)
-	fmt.Printf("done after %v", time.Since(start))
-}
-func ExampleOrByte() {
-	sig := func(after time.Duration) <-chan byte {
-		c := make(chan byte)
-		go func() {
-			defer close(c)
-			time.Sleep(after)
-		}()
-		return c
-	}
-
-	start := time.Now()
-	channels := []<-chan byte{
-		sig(2 * time.Hour),
-		sig(5 * time.Minute),
-		sig(1 * time.Second), // the close message of this channel will close the whole channels
-		sig(1 * time.Hour),
-		sig(1 * time.Minute),
-	}
-
-	<-pipeline.OrByte(channels...)
-	fmt.Printf("done after %v", time.Since(start))
-}
-
-func ExampleOrString() {
-	sig := func(after time.Duration) <-chan string {
-		c := make(chan string)
-		go func() {
-			defer close(c)
-			time.Sleep(after)
-		}()
-		return c
-	}
-
-	start := time.Now()
-	channels := []<-chan string{
-		sig(2 * time.Hour),
-		sig(5 * time.Minute),
-		sig(1 * time.Second), // the close message of this channel will close the whole channels
-		sig(1 * time.Hour),
-		sig(1 * time.Minute),
-	}
-
-	<-pipeline.OrString(channels...)
-	fmt.Printf("done after %v", time.Since(start))
-}
-
-func ExampleOrInt() {
-	sig := func(after time.Duration) <-chan int {
-		c := make(chan int)
-		go func() {
-			defer close(c)
-			time.Sleep(after)
-		}()
-		return c
-	}
-
-	start := time.Now()
-	channels := []<-chan int{
-		sig(2 * time.Hour),
-		sig(5 * time.Minute),
-		sig(1 * time.Second), // the close message of this channel will close the whole channels
-		sig(1 * time.Hour),
-		sig(1 * time.Minute),
-	}
-
-	<-pipeline.OrInt(channels...)
-	fmt.Printf("done after %v", time.Since(start))
-}
-
-func ExampleOrUint64() {
-	sig := func(after time.Duration) <-chan uint64 {
-		c := make(chan uint64)
-		go func() {
-			defer close(c)
-			time.Sleep(after)
-		}()
-		return c
-	}
-
-	start := time.Now()
-	channels := []<-chan uint64{
-		sig(2 * time.Hour),
-		sig(5 * time.Minute),
-		sig(1 * time.Second), // the close message of this channel will close the whole channels
-		sig(1 * time.Hour),
-		sig(1 * time.Minute),
-	}
-
-	<-pipeline.OrUint64(channels...)
-	fmt.Printf("done after %v", time.Since(start))
-}
-
-func ExampleOrFloat32() {
-	sig := func(after time.Duration) <-chan float32 {
-		c := make(chan float32)
-		go func() {
-			defer close(c)
-			time.Sleep(after)
-		}()
-		return c
-	}
-
-	start := time.Now()
-	channels := []<-chan float32{
-		sig(2 * time.Hour),
-		sig(5 * time.Minute),
-		sig(1 * time.Second), // the close message of this channel will close the whole channels
-		sig(1 * time.Hour),
-		sig(1 * time.Minute),
-	}
-
-	<-pipeline.OrFloat32(channels...)
-	fmt.Printf("done after %v", time.Since(start))
-}
-
-func ExampleOrFloat64() {
-	sig := func(after time.Duration) <-chan float64 {
-		c := make(chan float64)
-		go func() {
-			defer close(c)
-			time.Sleep(after)
-		}()
-		return c
-	}
-
-	start := time.Now()
-	channels := []<-chan float64{
-		sig(2 * time.Hour),
-		sig(5 * time.Minute),
-		sig(1 * time.Second), // the close message of this channel will close the whole channels
-		sig(1 * time.Hour),
-		sig(1 * time.Minute),
-	}
-
-	<-pipeline.OrFloat64(channels...)
+	<-pipeline.OrGenType(channels...)
 	fmt.Printf("done after %v", time.Since(start))
 }
